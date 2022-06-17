bedec7446146699b6a3cb9a938ed853869e506d4
Wugawuga <dnr3655@naver.com>
2022-06-17 11:18:03 +0900

    create Project

---
+++ src/main/java/com/mileage/wuga/WugaApplication.java
@@ -0,0 +1,13 @@
+package com.mileage.wuga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WugaApplication.class, args);
+	}
+
+}
